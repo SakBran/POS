d287fa2d213db231d23a3d8d5a8bd3cce77e28fa
Sao Bran Aung <saobran.aung61@gmail.com>
2025-07-15 11:21:55 +0630

    feat: update UI based on has variants or not

--- Frontend/src/pages/Product/Steps/ProductStep.tsx
+++ Frontend/src/pages/Product/Steps/ProductStep.tsx
@@ -1,11 +1,37 @@
-import { Form, Input, Select } from 'antd';
+import { Form, FormInstance, Input, Select } from 'antd';
 import BasicStepForm from '../../../components/My Components/StepForm/BasicStepForm';
+import React, { useEffect } from 'react';
+import axiosInstance from '../../../services/AxiosInstance';
+import { useParams } from 'react-router-dom';
 
 type StepProps = {
   current: number;
   setCurrent: (current: number) => void;
 };
+
+const onLoadDataFetch = async (
+  productId: string,
+  formRef: React.RefObject<FormInstance<any> | null>
+) => {
+  const url = `Product/GetProductWithVariants/${productId}`;
+  try {
+    const resp = await axiosInstance.get(url);
+    const productData = await resp.data;
+    formRef.current?.setFieldsValue(productData);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
 const ProductStep: React.FC<StepProps> = ({ current, setCurrent }) => {
+  const { id } = useParams();
+  const formRef = React.useRef<FormInstance>(null);
+
+  useEffect(() => {
+    if (id) {
+      onLoadDataFetch(id, formRef);
+    }
+  }, [id]);
   return (
     <BasicStepForm current={current} setCurrent={setCurrent} APIURL={'Product'}>
       <Form.Item label="Name" name="name">
@@ -14,15 +40,25 @@ const ProductStep: React.FC<StepProps> = ({ current, setCurrent }) => {
       <Form.Item label="Barcode" name="barcode">
         <Input />
       </Form.Item>
-
-      <Form.Item label="Cost Price" name="costPrice">
-        <Input type="number" />
+      <Form.Item name="hasVariants" hidden>
+        <Input type="hidden" />
       </Form.Item>
-      <Form.Item label="Retail Price" name="retailPrice">
-        <Input type="number" />
-      </Form.Item>
-      <Form.Item label="Wholesale Price" name="wholesalePrice">
-        <Input type="number" />
+      <Form.Item shouldUpdate noStyle>
+        {({ getFieldValue }) =>
+          !getFieldValue('hasVariants') ? (
+            <>
+              <Form.Item label="Cost Price" name="costPrice">
+                <Input type="number" />
+              </Form.Item>
+              <Form.Item label="Retail Price" name="retailPrice">
+                <Input type="number" />
+              </Form.Item>
+              <Form.Item label="Wholesale Price" name="wholesalePrice">
+                <Input type="number" />
+              </Form.Item>
+            </>
+          ) : null
+        }
       </Form.Item>
 
       <Form.Item label="Category" name="categoryId">
